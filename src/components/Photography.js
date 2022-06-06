4c5c93085e760ae9d0e01afeb279079a9610ced0
klausMistlberger <klaus.mistlberger@pm.me>
2022-06-06 23:16:36 +0200

    photo slideshow

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -8,54 +8,59 @@ export default function Photography(props) {
     return (
         <div className="photography">
             <h2>Photography</h2>
-            <p>
+            <p className="photo-description">
                 I'm very passionate about analog photography and develop my films myself. Here I just present a small amount of the pictures I've taken. Feel free to take a look around at my <a href="https://www.instagram.com/klaus.mistlberger/" target="_blank" rel="noreferrer">Instagram</a> or <a href="https://www.lomography.com/homes/threolin" target="_blank" rel="noreferrer">Lomo Home</a>.
             </p>
             
-            <div className="photo-equipment">
-                <h3 className="equipment-list">Equipment</h3>
-                <h4 className="equipment-list">Cameras</h4>
-                <ul className="equipment-list">
-                    <li>Nikon F2</li>
-                    <li>Lomo LC-A</li>
-                    <li>Lomography Sprocket Rocket</li>
-                    <li>Lomo’Instant Automat Glass</li>
-                    <li>Fujifilm Instax Mini 90 Neo Classic</li>
-                </ul>
-                <br />
-                <h4 className="equipment-list">Films</h4>
-                <ul className="equipment-list">
-                    <li className="listbreak">CineStill 800T</li>
-                    <li>Fomapan 100</li>
-                    <li className="listbreak">Fomapan 400</li>
-                    <li>Ilford Delta 100</li>
-                    <li>Ilford Delta 400</li>
-                    <li>Ilford Delta 3200</li>
-                    <li className="listbreak">Ilford HP5+</li>
-                    <li>Kodak Ektar 100</li>
-                    <li>Kodak Gold 200</li>
-                    <li className="listbreak">Kodak Portra 400</li>
-                    <li>LomoChrome Metropolis</li>
-                    <li>LomoChrome Purple</li>
-                    <li>Lomography Color Negative 400</li>
-                    <li className="listbreak">Lomography Color Negative 800</li>
-                    <li>Revolog Kolor</li>
-                </ul>
-                <br />
-                <h4 className="equipment-list">Chemicals</h4>
-                <ul className="equipment-list">
-                    <li>Rodinal</li>
-                    <li>Tetenal C41</li>
-                </ul>
-            </div>
-
-            <div>
+            {
+                !props.slideshow && 
                 <Photo 
                     photos={photolist}
                     openSlideshow={props.openSlideshow}
                 />
+            }
+            
 
-            </div>
+            {
+                !props.slideshow && 
+                <div className="photo-equipment">
+                    <h3 className="equipment-list">Equipment</h3>
+                    <h4 className="equipment-list">Cameras</h4>
+                    <ul className="equipment-list">
+                        <li>Nikon F2</li>
+                        <li>Lomo LC-A</li>
+                        <li>Lomography Sprocket Rocket</li>
+                        <li>Lomo’Instant Automat Glass</li>
+                        <li>Fujifilm Instax Mini 90 Neo Classic</li>
+                    </ul>
+                    <br />
+                    <h4 className="equipment-list">Films</h4>
+                    <ul className="equipment-list">
+                        <li className="listbreak">CineStill 800T</li>
+                        <li>Fomapan 100</li>
+                        <li className="listbreak">Fomapan 400</li>
+                        <li>Ilford Delta 100</li>
+                        <li>Ilford Delta 400</li>
+                        <li>Ilford Delta 3200</li>
+                        <li className="listbreak">Ilford HP5+</li>
+                        <li>Kodak Ektar 100</li>
+                        <li>Kodak Gold 200</li>
+                        <li className="listbreak">Kodak Portra 400</li>
+                        <li>LomoChrome Metropolis</li>
+                        <li>LomoChrome Purple</li>
+                        <li>Lomography Color Negative 400</li>
+                        <li className="listbreak">Lomography Color Negative 800</li>
+                        <li>Revolog Kolor</li>
+                    </ul>
+                    <br />
+                    <h4 className="equipment-list">Chemicals</h4>
+                    <ul className="equipment-list">
+                        <li>Rodinal</li>
+                        <li>Tetenal C41</li>
+                    </ul>
+                </div>
+            }
+            
 
         </div>
     );
--- src/components/photolist.js
+++ src/components/photolist.js
@@ -1,16 +1,16 @@
 const photolist = [
-    '2112_KE100_13.jpg',
-    '2112_KE100_14.jpg',
-    '2106_KP400_34.jpg',
-    '2112_KE100_28.jpg',
-    '2107_KP400_17.jpg',
-    'Film7819_15a.jpg',
-    'Film8096_29.jpg',
-    '2201_LCM400_05.jpg',
-    '2107_LCN800_SR_008.jpg',
-    '2112_KE100_34.jpg',
-    '2105_KG200_008.jpg',
-    '2108_ID3200_26.jpg',
+    '2112_KE100_13.jpeg',
+    '2112_KE100_14.jpeg',
+    '2106_KP400_34.jpeg',
+    '2112_KE100_28.jpeg',
+    '2107_KP400_17.jpeg',
+    'Film7819_15a.jpeg',
+    'Film8096_29.jpeg',
+    '2201_LCM400_05.jpeg',
+    '2107_LCN800_SR_008.jpeg',
+    '2112_KE100_34.jpeg',
+    '2105_KG200_008.jpeg',
+    '2108_ID3200_26.jpeg',
 ];
 
 export default photolist;
