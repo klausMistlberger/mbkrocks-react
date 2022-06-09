8399f4dcad65862be599c8dd469c08bd42252738
klausMistlberger <klaus.mistlberger@pm.me>
2022-06-09 12:51:00 +0200

    key

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
 export default function Photo(props) {
-    // console.log( props );
 
     const photos = props.photos.map( (element, index) => {
-
         const style = {
             backgroundImage: `url(./photos/${element})`
         };
-        
+
         return (
-            <div className="photo-container">
+            <div className="photo-container" key={element}>
                 <div 
-                    key={element}
                     data-index={index}
                     data-image={element}
                     className="photo" 
